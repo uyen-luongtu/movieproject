d506007284871f93fb13985bddc0bb4dcfeeeb10
uyen-luongtu <ltu2601@gmail.com>
2022-04-15 17:16:53 +0700

    change domain api

--- src/util/settings/config.js
+++ src/util/settings/config.js
@@ -1,5 +1,5 @@
 
-export const DOMAIN = 'https://movie0706.cybersoft.edu.vn'
+export const DOMAIN = 'http://movieapi.cyberlearn.vn/api'
 export const USER_LOGIN = 'UserAccount'
 export const TOKEN = 'accessToken'
 export const GROUP_ID = 'GP01'
