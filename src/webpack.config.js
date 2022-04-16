1f691c57201db7518eaddadddfdce66cd6c8788e
uyen-luongtu <ltu2601@gmail.com>
2022-04-16 21:23:09 +0700

    webpack

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,9 +1,1 @@
-const path = require("path");
-
-module.exports = {
-    entry: "./src/index.js",
-    output: {
-        path: path.join(__dirname, "/dist"),
-        filename: "index_bundle.js"
-    }
-}
+module.exports = { resolve: { modules: ["../node_modules/"] } }
